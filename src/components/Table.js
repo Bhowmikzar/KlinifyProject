f2707aa1f42c6164ea4d1fba8fda6a8ef72a55af
Arijit Bhowmik <102834292+Bhowmikzar@users.noreply.github.com>
2022-10-17 15:30:34 +0530

    Update Table.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -26,7 +26,7 @@ export const Table = () => {
 
   const handleDragEnd = (results) => {
     let tempuser = [...data];
-    let slectedRow = tempuser.splic(results.source.index, 1);
+    let slectedRow = tempuser.splice(results.source.index, 1);
     tempuser.splice(results.destination.index, 0, slectedRow);
   };
 
