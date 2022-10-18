cf6f925d59d71ec1d90a79fee8c9e7a197dee302
Arijit Bhowmik <102834292+Bhowmikzar@users.noreply.github.com>
2022-10-19 01:50:27 +0530

    Update Table.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,4 @@
-import React, { useMemo , useState } from "react";
+import React, { useMemo , useState , useEffect } from "react";
 import { useTable, useFilters, useSortBy } from "react-table";
 import patient from "./patient.json";
 import { COLUMNS } from "./columns";
